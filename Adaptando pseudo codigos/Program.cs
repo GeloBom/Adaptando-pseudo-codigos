c76a99a28e5c4d6d9521a8dd970c76e2648d4cc8
lghs6 <lghs6@DESKTOP-37R8PBR>
2023-07-27 12:10:45 -0300

    Add project files.

---
+++ Adaptando pseudo codigos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adaptando_pseudo_codigos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            salario_mensal = input('Qual é o seu salário mensal?')
+            //horas_trabalhadas_por_mes = input('Quantas horas trabalha por mes?')
+            //valor_hora = int(salario_mensal) / int(horas_trabalhadas_por_mes)
+            //print(valor_hora)
+
+            
+            string Salario_mensal = Input("Qual seu salario?");
+            string Horas_trabalhadas = Input("Quantas horas vc trabalha por mes?");
+            int Valor_hora = int.Parse(Salario_mensal) /int.Parse(Horas_trabalhadas);
+            Console.WriteLine(Valor_hora);
+
+        }
+        static string Input(string Mensagem)
+        {       
+            Console.WriteLine(Mensagem);
+            return Console.ReadLine();
+        }
+    }
+}
+
